0b6c507ff29e421ddeae2c25f91a94b449037df3
Adam Staples <bent.paperclip27@gmail.com>
2020-08-24 09:55:08 -0600

    Added additional tools, fixed renamed extensions.

--- vs-code/quick-setup-vscode.ps1
+++ vs-code/quick-setup-vscode.ps1
@@ -2,14 +2,17 @@ Write-Host '----- Setting up Visual Studio Code.'
 scoop install vscode
 
 echo "----- Installing VS Code extensions for language support and other useful things"
-code --install-extension ms-vscode.csharp --force
+ms-dotnettools.csharp
+4ops.terraform
+golang.go
+code --install-extension ms-dotnettools.csharp --force
 code --install-extension k--kato.docomment --force
 code --install-extension jchannon.csharpextensions --force
-code --install-extension ms-vscode.go --force
+code --install-extension golang.go --force
 code --install-extension ms-python.python --force
 code --install-extension ms-vscode.powershell --force
 code --install-extension ms-azuretools.vscode-docker --force
-code --install-extension mauve.terraform --force
+code --install-extension 4ops.terraform --force
 code --install-extension github.vscode-pull-request-github --force
 code --install-extension bigous.vscode-multi-line-tricks --force
 code --install-extension eriklynd.json-tools --force
@@ -22,14 +25,18 @@ code --install-extension eg2.vscode-npm-script --force
 code --install-extension jmrog.vscode-nuget-package-manager --force
 code --install-extension mohsen1.prettify-json --force
 code --install-extension tyriar.shell-launcher --force
-code --install-extension hediet.vscode-drawio
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension tabnine.tabnine-vscode
-code --install-extension hbenl.vscode-test-explorer
-code --install-extension formulahendry.dotnet-test-explorer
+code --install-extension hediet.vscode-drawio --force
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack --force
+code --install-extension ms-vscode-remote.remote-ssh --force
+code --install-extension ms-vscode-remote.remote-ssh-edit --force
+code --install-extension dbaeumer.vscode-eslint --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension tabnine.tabnine-vscode --force
+code --install-extension hbenl.vscode-test-explorer --force
+code --install-extension formulahendry.dotnet-test-explorer --force
+code --install-extension k--kato.docomment --force
+code --install-extension msjsdiag.debugger-for-chrome --force
+code --install-extension ms-vsliveshare.vsliveshare-pack --force
+code --install-extension eg2.vscode-npm-script --force
 # Windows only
-code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.remote-wsl --force
